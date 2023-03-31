4a3cf7fdf6afde1fe78cfc344ad61fa2f0e0a573
layla bano <123286276+laylask@users.noreply.github.com>
2023-03-31 14:28:39 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 // Define an array of animation class names
 const animationClasses = ["animation-1", "animation-2", "animation-3"];
+
+// Get the text element
+const textElement = document.getElementById("text");
+
+// Remove any existing animation classes from the text element
+textElement.classList.remove(...animationClasses);
+
 // Generate a random index into the animationClasses array
 const randomIndex = Math.floor(Math.random() * animationClasses.length);
-// Get the text element and add the randomly chosen animation class
-const textElement = document.getElementById("text");
+
+// Add the randomly chosen animation class to the text element
 textElement.classList.add(animationClasses[randomIndex]);
